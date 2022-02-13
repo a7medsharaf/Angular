13133767412227ac9cf83eedf4dbd87eab3969da
a7medsharaf <a7med.sharaf@gmail.com>
2022-02-14 01:09:20 +0200

    h

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms';
+  defaultname:string='Ahmed';
 
   onSubmit(F:NgForm)
   {
