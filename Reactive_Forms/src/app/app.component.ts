eafbd5b2bc3aae4c2b26d8becfbc67eb1e57b81a
a7medsharaf <a7med.sharaf@gmail.com>
2022-02-14 23:53:12 +0200

    s

--- Reactive_Forms/src/app/app.component.ts
+++ Reactive_Forms/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  signupform:FormGroup;
+  ngOnInit(): void {
+    this.signupform=new FormGroup({
+      'username':new FormControl(null,Validators.required),
+      'useremail':new FormControl(null,[Validators.required,Validators.email]),
+      'usergender':new FormControl('Female')
+    })
+  }
   title = 'Reactive_Forms';
+   GenderArr:string[]=['Male','Female'];
+   onsubmit()
+   {
+     if(this.signupform.valid)
+     {
+      console.log(this.signupform);
+     }
+     else{
+       console.log('Not Valid');
+     }
+   }
 }
