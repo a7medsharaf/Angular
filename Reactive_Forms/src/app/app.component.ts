6a51429906d4c2711673d40923e1a167db85184f
a7medsharaf <a7med.sharaf@gmail.com>
2022-02-15 22:36:47 +0200

    d

--- Reactive_Forms/src/app/app.component.ts
+++ Reactive_Forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +7,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  forbiddennames:string[]=['khawal','3ars'];
+  errorsarr:string[]=[];
   signupform:FormGroup;
   ngOnInit(): void {
     this.signupform=new FormGroup({
-      'username':new FormControl(null,Validators.required),
+
+      'userData':new FormGroup({
+     'username':new FormControl(null,[Validators.required,this.forbidden.bind(this)]),
       'useremail':new FormControl(null,[Validators.required,Validators.email]),
       'usergender':new FormControl('Female')
+    }),
+    'hobbies':new FormArray([])
     })
   }
   title = 'Reactive_Forms';
@@ -25,6 +32,58 @@ export class AppComponent implements OnInit {
      }
      else{
        console.log('Not Valid');
+       console.log(this.signupform);
+
+     
+
      }
+   }
+
+   onhobbies()
+   {
+     const control=new FormControl(null,Validators.required);
+     (<FormArray>  this.signupform.get('hobbies')).push(control);
+   }
+
+   usernamevalidators():boolean
+   {
+     let res:boolean=false;
+     this.errorsarr=[];
+     if(this.signupform.get('userData.username')!.status=='INVALID')
+     {
+      if(this.signupform.get('userData.username')!.errors!['nameisforbidden'] ===true)
+      {
+          this.errorsarr.push('Name is forbidden');
+          console.log(this.errorsarr)
+          res=true;
+      }
+     }
+   
+    else
+    {
+      console.log(this.errorsarr)
+      res=false;
+    }
+    return res;
+   }
+
+   get_array()
+   {
+    return this.signupform.get('hobbies') as FormArray
+   }
+
+   forbidden(C:FormControl)
+   {
+      if(this.forbiddennames.includes(C.value) )
+      {
+        console.log(this.forbiddennames.indexOf(C.value)+' true');
+        return {'nameisforbidden':true}
+      }
+      else
+      {
+        console.log('false');
+        return null;
+      }
+     
    }
 }
